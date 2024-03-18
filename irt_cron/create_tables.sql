7eb03502dadf8d0149fb48ddbfa795ea248611d7
Ronald Großmann <dev@ronaldg.de>
2024-03-18 22:56:14 +0100

    Updated config examples
    Updated the configs to reflect the new split of the road licence by iRacing

--- irt_cron/create_tables.sql
+++ irt_cron/create_tables.sql
@@ -73,3 +73,35 @@ CREATE TABLE IF NOT EXISTS `road` (
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `sports_car` (
+  `id` mediumint(8) unsigned NOT NULL,
+  `driver` varchar(255) NOT NULL,
+  `location` varchar(2) NOT NULL,
+  `club` varchar(32) NOT NULL,
+  `starts` smallint(5) unsigned NOT NULL,
+  `wins` smallint(5) unsigned NOT NULL,
+  `avg_start` tinyint(3) unsigned NOT NULL,
+  `avg_finish` tinyint(4) NOT NULL,
+  `avg_inc` decimal(5,2) unsigned NOT NULL,
+  `safetyrating` varchar(6) NOT NULL,
+  `irating` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `sports_car` (
+  `id` mediumint(8) unsigned NOT NULL,
+  `driver` varchar(255) NOT NULL,
+  `location` varchar(2) NOT NULL,
+  `club` varchar(32) NOT NULL,
+  `starts` smallint(5) unsigned NOT NULL,
+  `wins` smallint(5) unsigned NOT NULL,
+  `avg_start` tinyint(3) unsigned NOT NULL,
+  `avg_finish` tinyint(4) NOT NULL,
+  `avg_inc` decimal(5,2) unsigned NOT NULL,
+  `safetyrating` varchar(6) NOT NULL,
+  `irating` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
